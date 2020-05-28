87d34e0ed6a2ce174b6adfe6ee51f4b521a10067
vincent.chen <vincent79715@gmail.com>
2020-05-28 18:13:16 +0800

    Update python_GUI_webcam.py

--- python_GUI_webcam.py
+++ python_GUI_webcam.py
@@ -4,11 +4,6 @@ import time
 from tkinter import *
 from PIL import Image,ImageTk
 
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
-z,continue_time,interval_time=1,1,1
-start_time,last_time = time.time(),time.time()
-
 def read_camera():
     global frame
     ret, frame = cap.read()
@@ -16,7 +11,6 @@ def read_camera():
     picturebox.config(image=img)
     picturebox.image=img
     root.after(1, read_camera)
-
 def continue_save(t1=-1,t2=-1):
     global last_time,z
     if t1==-1 : t1=interval_time
@@ -28,44 +22,43 @@ def continue_save(t1=-1,t2=-1):
         label_message.config(text='save '+str(z).zfill(4)+".jpg")
     if time.time()-start_time < t2:
         root.after(1, continue_save)
-
-def keypress(event=None):
+def KeyPress(event=None):
     key = event.keysym
     print(f"{event.keysym}:{event.char}:{event.keycode}")
     if key=='q' or key=='Escape': quit()
     elif key=='s' or key=='space': button_save_click()
     elif key=='c': button_continue_click()
-    
 def button_save_click():
     continue_save(0,0)
-
 def button_continue_click():
     global start_time
     start_time = time.time()
     continue_save()
-
-def Scale1_fn(v):
+def scale_interval_scroll(v):
     global interval_time
     interval_time = 1/int(v)
     Scale1.config(label='save '+v+' image/sec')
-
-def Scale2_fn(v):
+def scale_continue_scroll(v):
     global continue_time
     continue_time = int(v)
     Scale2.config(label='continue save '+v+' sec')
-
 def quit():
     cap.release()
     root.destroy()
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+z,continue_time,interval_time=1,1,1
+start_time,last_time = time.time(),time.time()
+
 root = Tk()
 root.title("Capture tool")
 root.geometry("750x730+300+100")
-root.bind("<Key>",keypress)
+root.bind("<Key>",KeyPress)
 
-scale_interval = Scale(root, label='save 1 image/sec', from_=1, to=30, orient=HORIZONTAL,length=480, showvalue=0, tickinterval=2, resolution=1, command=Scale1_fn)
+scale_interval = Scale(root, label='save 1 image/sec', from_=1, to=30, orient=HORIZONTAL,length=480, showvalue=0, tickinterval=2, resolution=1, command=scale_interval_scroll)
 scale_interval.pack()
-scale_continue = Scale(root, label='continue save 1 sec', from_=1, to=60, orient=HORIZONTAL,length=480, showvalue=0, tickinterval=4, resolution=1, command=Scale2_fn)
+scale_continue = Scale(root, label='continue save 1 sec', from_=1, to=60, orient=HORIZONTAL,length=480, showvalue=0, tickinterval=4, resolution=1, command=scale_continue_scroll)
 scale_continue.pack()
 scale_interval.set(3)
 scale_continue.set(3)
