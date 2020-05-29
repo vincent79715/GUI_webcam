e1bc9a05a6e526386eb52cc91bccd48a761f237d
vincent.chen <vincent79715@gmail.com>
2020-05-29 23:32:59 +0800

    Update GUI_webcam.py

--- GUI_webcam.py
+++ GUI_webcam.py
@@ -2,55 +2,45 @@ import os
 import cv2
 import time
 import _thread
-import multiprocessing
 import numpy as np
+import multiprocessing as mp
 from tkinter import *
 from PIL import Image,ImageTk
 
 def read_camera():
-    global cap,run_p,last_time
+    global cap,run_p,last_time,bGet,z
+    showtext=''
     while bRuning:
+        t0 = time.time()
         ret, frame = cap.read()
+        t1 = time.time()
         # show error or image
         if ret :
-            temp = frame.copy()
-            if bGet and run_all>1:
-                T = int(continue_time*(1-run_p/run_all))
-                L = (60,350) if T>=10 else (200,350)
-                cv2.putText(temp , f'{T}' , L, cv2.FONT_HERSHEY_COMPLEX, 12, (255,255,255), 2)
-            img = ImageTk.PhotoImage(image=Image.fromarray(temp[:,:,::-1]))
+            if bGet and run_all>1 and run_p>1:
+                T = int((1-run_p/run_all)*100)
+                showtext = f'{T}'
+            img = ImageTk.PhotoImage(Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
         else:
             img = msg
             cap = cv2.VideoCapture(0)
+        t2 = time.time()
         # save image
         if ret and bGet and run_all>run_p and time.time()-last_time > interval_time:
-            while os.path.exists(f'{z:04}.jpg'): z+=1:
-            last_time += interval_time      
-            cv2.imwrite(f'{z:04}.jpg',frame)
+            last_time += interval_time     
+            _thread.start_new_thread(Save_image,(f'{z:04}.jpg',frame.copy()))			
             label_message.config(text=f'{z:04}.jpg')
-            run_p+=1
+            run_p,z = run_p+1,z+1
         if bGet and run_p>=run_all:
             bGet = False
             set_state(True)
-        cv2.waitKey(1)
-        picturebox.config(image=img)
+            showtext = ''
+        t3 = time.time()
+        picturebox.config(image=img,text=showtext)
         picturebox.image=img
-
-def continue_save(t1,num):
-    global last_time,z,run_p,run_all
-    run_p,run_all = 0,num
-    if t1!=0: set_state(False)
-    last_time = time.time()
-    while bRuning and run_p < num:
-        cv2.waitKey(1)
-        if time.time()-last_time > t1:
-            last_time +=t1
-            while os.path.exists(f'{z:04}.jpg'): z+=1
-            cv2.imwrite(f'{z:04}.jpg',frame)
-            label_message.config(text=f'{z:04}.jpg')
-            print(run_p,f'{(time.time()-last_time)*1000:.2f}ms',f'{t1*1000:.2f}ms')
-            run_p+=1
-    set_state(True)
+        t4 = time.time()
+        print(f'{(t4-t0)*1000:5.1f} , {(t1-t0)*1000:5.1f} , {(t2-t1)*1000:5.1f} , {(t3-t2)*1000:5.1f} , {(t4-t2)*1000:5.1f}')
+def Save_image(name,img):
+    cv2.imwrite(name,img)
 def KeyPress(event=None):
     key = event.keysym
     if key=='s' or key=='space': button_save_click()
@@ -94,7 +84,6 @@ def set_state(bstate):
         scale_continue['state'] = 'disable'
         button_save['state'] = 'disable'
         button_continue['state'] = 'disable'
-
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 root = Tk()
 root.title("Capture tool")
@@ -106,7 +95,7 @@ scale_interval.set(3)
 scale_continue = Scale(root, label='continue save 1 sec', from_=1, to=60, orient=HORIZONTAL,length=480, showvalue=0, tickinterval=4, command=scale_continue_scroll)
 scale_continue.grid(row=1,columnspan=2)
 scale_continue.set(3)
-picturebox = Label(root) 
+picturebox = Label(root,text = '', compound='center', font=("Times", 150),fg="white") 
 picturebox.grid(row=2,columnspan=2,padx=20)
 button_save = Button(root,text = 'save image',width=30, height=5,command = button_save_click)
 button_save.grid(row=3,column=0,padx=20,pady=5,sticky='w')
@@ -115,13 +104,18 @@ button_continue.grid(row=3,column=1,padx=20,pady=5,sticky='e')
 label_message = Label(root,text = '') 
 label_message.grid(row=4,columnspan=2,sticky='e')
 
+
+
 cap = cv2.VideoCapture(0)
 z,image_sec,continue_time=1,1,1
+while os.path.exists(f'{z:04}.jpg'): z+=1
 run_p,run_all=1,1
 start_time,last_time = time.time(),time.time()
+bRuning,bGet = True,False
 msg = np.zeros(640*480*3).reshape(480,640,3).astype(np.uint8)
 cv2.putText(msg , "Camera error" , (80,260), cv2.FONT_HERSHEY_COMPLEX, 2, (255,255,255), 2)
 msg = ImageTk.PhotoImage(image=Image.fromarray(msg))
-bRuning = True
+
 _thread.start_new_thread(read_camera,())
+
 root.mainloop()
