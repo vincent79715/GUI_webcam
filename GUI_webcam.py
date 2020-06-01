95960ebbc9c96ccb9418049a88eb10796e31866e
vincent.chen <vincent79715@gmail.com>
2020-06-01 10:46:34 +0800

    Update GUI_webcam.py

--- GUI_webcam.py
+++ GUI_webcam.py
@@ -8,24 +8,27 @@ from tkinter import *
 from PIL import Image,ImageTk
 
 def read_camera():
-    global cap,run_p,last_time,bGet,z,q
+    global cap,run_p,last_time,bGet,zdir,q
     showtext1,showtext2='',''
     while bRuning:
+        inum=1
         t0 = time.time()
         ret, frame = cap.read()
         # save image
         t1 = time.time()
         if ret and bGet and run_all>run_p and time.time()-last_time > interval_time:
             last_time += interval_time
-            while os.path.exists(f'{z:04}.jpg'): z+=1
-            showtext1 = f'{z:04}.jpg'
+            sdir = f'{zdir:03}/' if run_all>1 else ''
+            if not os.path.isdir(f'{zdir:03}'): os.mkdir(f'{zdir:03}')
+            while os.path.exists(f'{sdir}{inum:04}.jpg'): inum+=1
+            showtext1 = f'{sdir}{inum:04}.jpg'
             qsave.put([showtext1,frame*1])      
-            run_p,z = run_p+1,z+1
+            run_p = run_p+1
         if bGet and run_p>=run_all: set_state(True) #End
         # show error or image
         t2 = time.time()
         if ret:
-            showtext2 = f'{int((1-run_p/run_all)*100)}' if (bGet and run_p<run_all) else ''
+            showtext2 = f'{int((1-run_p/run_all)*continue_time)}' if (bGet and run_p<run_all) else ''
             qGUI.put([ret,cv2.cvtColor(frame, cv2.COLOR_BGR2RGB),showtext1,showtext2])
         else:
             qGUI.put([ret,[],' ',' '])
@@ -69,11 +72,13 @@ def button_save_click():
         last_time = time.time()-0.001
         set_state(False)
 def button_continue_click():
-    global last_time,run_p,run_all,interval_time
+    global last_time,run_p,run_all,interval_time,zdir
     if not bGet:
-        interval_time,run_p,run_all = 1/image_sec,0,continue_time*image_sec
+        interval_time,run_p,run_all,zdir = 1/image_sec,0,continue_time*image_sec,zdir+1
         last_time = time.time()-interval_time-0.001
         set_state(False)
+        while os.path.isdir(f'{zdir:03}'): zdir+=1
+        
 def scale_interval_scroll(v):
     global image_sec
     image_sec = int(v)
@@ -112,12 +117,12 @@ if __name__ == '__main__':
     root.bind("<Key>",KeyPress)
     root.protocol("WM_DELETE_WINDOW", Exit)
     
-    scale_interval = Scale(root, label='save 1 image/sec', from_=1, to=30, orient=HORIZONTAL,length=480, showvalue=0, tickinterval=2, command=scale_interval_scroll)
+    scale_interval = Scale(root, label='save 1 image/sec', from_=1, to=10, orient=HORIZONTAL,length=480, showvalue=0, tickinterval=1, command=scale_interval_scroll)
     scale_interval.grid(row=0,columnspan=2)
-    scale_interval.set(3)
+    scale_interval.set(10)
     scale_continue = Scale(root, label='continue save 1 sec', from_=1, to=60, orient=HORIZONTAL,length=480, showvalue=0, tickinterval=4, command=scale_continue_scroll)
     scale_continue.grid(row=1,columnspan=2)
-    scale_continue.set(3)
+    scale_continue.set(10)
     picturebox = Label(root,text = '', compound='center', font=("Times", 150),fg="white") 
     picturebox.grid(row=2,columnspan=2,padx=20)
     button_save = Button(root,text = 'save image',width=30, height=5,command = button_save_click)
@@ -127,15 +132,16 @@ if __name__ == '__main__':
     label_message = Label(root,text = '') 
     label_message.grid(row=4,columnspan=2,sticky='e')
 
-    cap = cv2.VideoCapture(0) 
-    z,image_sec,continue_time=1,1,1
-    
     run_p,run_all=1,1
     last_time = time.time()
     bRuning,bGet = True,False
     msg = np.zeros(640*480*3).reshape(480,640,3).astype(np.uint8)
     cv2.putText(msg , "Camera error" , (80,260), cv2.FONT_HERSHEY_COMPLEX, 2, (255,255,255), 2)
     msg = ImageTk.PhotoImage(image=Image.fromarray(msg))
+
+    cap = cv2.VideoCapture(0) 
+    zdir,image_sec,continue_time=0,1,1
+
 
     qGUI = mp.Queue()
     qsave = mp.Queue()
